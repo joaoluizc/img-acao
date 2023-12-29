d8a50530fb6f8bc773849efdb66b329fddf26f0c
joaoluizc <joaocoelho.tdm@gmail.com>
2023-12-29 16:13:55 -0300

    adicionando pontos no objeto e dado no App

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,21 @@ import Card from './Card';
 
 function App() {
   const [action, setAction] = useState('');
-
-  const newCard = () => {
-    const newCategory = data.categories[Math.floor(Math.random() * data.categories.length)];
-    const newAction = data[newCategory][Math.floor(Math.random() * data[newCategory].length)];
-    setAction(newAction);
-  };
+  const [dice, setDice] = useState('');
 
   const rollDice = () => {
     const newCategory = data.categories[Math.floor(Math.random() * data.categories.length)];
-    alert(`Você tirou ${newCategory} no dado!`)
+    const category = data.categoriesMapping[newCategory];
+    setDice(category);
   }
 
   return (
     <>
       <h1>Imagem & Ação</h1>
       <div>
-        <h3>Categoria</h3>
-        <div id="category">{ action.type }</div>
+        <h3>Dado</h3>
+        <div id="action">{ dice }</div>
       </div>
-      <div>
-        <h3>Ação</h3>
-        <div id="action">{ action.action }</div>
-      </div>
-      <button onClick={ newCard }>Gerar</button>
       <button onClick={ rollDice }>Rolar Dado</button>
       <Card />
     </>
